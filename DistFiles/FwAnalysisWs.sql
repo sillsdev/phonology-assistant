e66912f876616fee60e784b3cdd9b36837adee3a
david_olson <devnull@localhost>
2007-05-16 21:07:09 +0000

    Don't need these anymore. They're in a single XML file.

--- DistFiles/FwAnalysisWs.sql
+++
@@ -1,4 +0,0 @@
-select * from
-LanguageProject_AnalysisWritingSystems lp (readuncommitted)
-INNER JOIN LgWritingSystem_Name lg (readuncommitted)
-ON lp.Dst = lg.Obj
--- DistFiles/FwVernacularWs.sql
+++
@@ -1,4 +0,0 @@
-select * from
-LanguageProject_VernacularWritingSystems lp (readuncommitted)
-INNER JOIN LgWritingSystem_Name lg (readuncommitted)
-ON lp.Dst = lg.Obj
