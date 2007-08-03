ca4e85a020a2506423254fd244adff2aa17f1b69
david_olson <devnull@localhost>
2007-08-03 20:12:45 +0000

    Tweaked the line spacing in the RTF record view a bit more.

--- src/PaControls/RtfRecordView.cs
+++ src/PaControls/RtfRecordView.cs
@@ -352,7 +352,7 @@ namespace SIL.Pa.Controls
 				m_lineSpacing = Math.Max(m_lineSpacing, ((m_maxFontSize / 2) * 1440) / 72);
 			}
 
-			m_lineSpacing += 40; // Add 40 twips for the sake of acenders and decenders.
+			m_lineSpacing += 50; // Add 50 twips for the sake of acenders and decenders.
 		}
 
 		/// ------------------------------------------------------------------------------------
