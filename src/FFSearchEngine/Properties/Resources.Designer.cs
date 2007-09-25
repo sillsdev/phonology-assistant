4459824021c5d98d92e0a8c212082464b23be8bc
david_olson <devnull@localhost>
2007-09-25 18:10:07 +0000

    PA-670: Fixed two error messages that had two spaces between a couple of words.

--- src/FFSearchEngine/Properties/Resources.Designer.cs
+++ src/FFSearchEngine/Properties/Resources.Designer.cs
@@ -241,7 +241,7 @@ namespace SIL.Pa.FFSearchEngine.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to When the zero-or-more symbol (*) is present in the preceding environment, it must be  at the beginning..
+        ///   Looks up a localized string similar to When the zero-or-more symbol (*) is present in the preceding environment, it must be at the beginning..
         /// </summary>
         internal static string kstidZeroOrMoreBeginningErr {
             get {
@@ -250,7 +250,7 @@ namespace SIL.Pa.FFSearchEngine.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to When the zero-or-more symbol (*) is present in the following environment, it must be  at the end..
+        ///   Looks up a localized string similar to When the zero-or-more symbol (*) is present in the following environment, it must be at the end..
         /// </summary>
         internal static string kstidZeroOrMoreEndingErr {
             get {
