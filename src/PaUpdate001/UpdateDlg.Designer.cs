930885d860cdd4b3925923cac3476e8193d1ad8b
david_olson <devnull@localhost>
2008-05-23 15:08:18 +0000

    PA-930: Fixed the queries used to get FieldWorks data from an FW database. Created an updating program to update those files on users' computers.

---
+++ src/PaUpdate001/UpdateDlg.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SIL.Pa.Updates
+{
+	partial class UpdateDlg
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateDlg));
+			this.lblAdminMsg = new System.Windows.Forms.Label();
+			this.lblUpdateMsg = new System.Windows.Forms.Label();
+			this.btnUpdate = new System.Windows.Forms.Button();
+			this.btnClose = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblAdminMsg
+			// 
+			resources.ApplyResources(this.lblAdminMsg, "lblAdminMsg");
+			this.lblAdminMsg.Name = "lblAdminMsg";
+			// 
+			// lblUpdateMsg
+			// 
+			resources.ApplyResources(this.lblUpdateMsg, "lblUpdateMsg");
+			this.lblUpdateMsg.Name = "lblUpdateMsg";
+			// 
+			// btnUpdate
+			// 
+			resources.ApplyResources(this.btnUpdate, "btnUpdate");
+			this.btnUpdate.Name = "btnUpdate";
+			this.btnUpdate.UseVisualStyleBackColor = true;
+			this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+			// 
+			// btnClose
+			// 
+			resources.ApplyResources(this.btnClose, "btnClose");
+			this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btnClose.Name = "btnClose";
+			this.btnClose.UseVisualStyleBackColor = true;
+			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+			// 
+			// UpdateDlg
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.btnClose;
+			this.Controls.Add(this.btnClose);
+			this.Controls.Add(this.btnUpdate);
+			this.Controls.Add(this.lblUpdateMsg);
+			this.Controls.Add(this.lblAdminMsg);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "UpdateDlg";
+			this.ShowIcon = false;
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblAdminMsg;
+		private System.Windows.Forms.Label lblUpdateMsg;
+		private System.Windows.Forms.Button btnUpdate;
+		private System.Windows.Forms.Button btnClose;
+	}
+}
+
---
+++ src/PaUpdate001/UpdateDlg.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace SIL.Pa.Updates
+{
+	/// ----------------------------------------------------------------------------------------
+	/// <summary>
+	/// The updater updates the FieldWorks query file (xml file) in the PA installation
+	/// folder. There was a bug in the query found a couple of weeks after the corporate
+	/// release. See PA-930 for more information. This update will replace the old query
+	/// file with a fixed version. In addition, this updater should be used to provide
+	/// the query file that will allow users of FieldWorks versions 5.3 or later to see
+	/// their FieldWorks data in PA.
+	/// </summary>
+	/// ----------------------------------------------------------------------------------------
+	public partial class UpdateDlg : Form
+	{
+		private string[] m_filesToUpdate =
+			new string[] {"FwSQLQueries.xml", "FwSQLQueriesShortNames.xml"};
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		public UpdateDlg()
+		{
+			InitializeComponent();
+			string msg = Properties.Resources.kstidUpdateMsg;
+			lblUpdateMsg.Text = msg.Replace("\\n", "\n"); 
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		private void btnClose_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+		
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		private void btnUpdate_Click(object sender, EventArgs e)
+		{
+			btnUpdate.Visible = false;
+			if (!DoUpdate())
+			{
+				btnUpdate.Visible = true;
+				return;
+			}
+
+			lblAdminMsg.Visible = false;
+			lblUpdateMsg.Text = Properties.Resources.kstidUpdateCompleteMsg;
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		private bool DoUpdate()
+		{
+			string paPath = PaPathFinder.GetPath();
+			if (paPath == null)
+				return false;
+
+			foreach (string file in m_filesToUpdate)
+				UpdateFile(paPath, file);
+
+			return true;
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		private void UpdateFile(string path, string file)
+		{
+			string fullPath = Path.Combine(path, file);
+
+			// If the file exists, then back it up.
+			if (File.Exists(fullPath))
+			{
+				string oldFile = Path.Combine(path, file);
+				while (File.Exists(oldFile))
+					oldFile += ".bak";
+
+				File.Move(fullPath, oldFile);
+			}
+
+			// Use the file name without its extension as the name of the resource.
+			// Then get the string resource that will be the contents of the new file.
+			// Then write the new file.
+			string resName = Path.GetFileNameWithoutExtension(file);
+			string newFileContent = Properties.Resources.ResourceManager.GetString(resName);
+			File.WriteAllText(fullPath, newFileContent, Encoding.UTF8);
+		}
+	}
+}
