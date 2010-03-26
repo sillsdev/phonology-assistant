f060b36796dfc8da3e21aad57a582cc0f86ae649
david_olson <devnull@localhost>
2010-03-26 19:46:20 +0000

    Initial check-in.

---
+++ src/Pa/Processing/Pipeline.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace SIL.Pa.Processing
+{
+	/// <summary>
+	/// Corresponds to one pipeline element in the Processing.xml file.
+	/// </summary>
+	public class Pipeline
+	{
+		private List<Step> m_stepList;
+
+		// In the Processing.xml file, each pipeline and its children are in the XProc namespace.
+		// However, the file does not necessarily specify the p: prefix on the inndividual elements.
+		static private XmlNameTable s_nameTable = null;
+		static private XmlNamespaceManager s_namespaceManager = null;
+		static private readonly string s_NamespacePrefix = "p";
+		static private readonly string s_NamespaceURI = "http://www.w3.org/ns/xproc";
+
+		// navigator: The parent process element containing the pipeline element.
+		// processingFolderPath: Full path to folder containing the *.xsl files.
+		// pipeline: Initially null, returns the object if this function returns true.
+		static public bool Create(XPathNavigator navigator, string processingFolderPath,
+			ref Pipeline pipeline)
+		{
+			Debug.Assert(pipeline == null);
+
+			// Create nameTable and namespaceManager once, when first needed.
+			// DAVID: Is this okay, and is there a better way to do it in C#?
+			if (s_namespaceManager == null)
+			{
+				s_nameTable = new NameTable();
+				s_namespaceManager = new XmlNamespaceManager(s_nameTable);
+				s_namespaceManager.AddNamespace(s_NamespacePrefix, s_NamespaceURI);
+			}
+
+			navigator = navigator.SelectSingleNode("p:pipeline", s_namespaceManager);
+			if (navigator == null)
+				return false;
+
+			List<Step> stepList = new List<Step>();
+			XPathNodeIterator iterator = navigator.Select("p:xslt", s_namespaceManager);
+			while (iterator.MoveNext())
+			{
+				XPathNavigator node = iterator.Current;
+				Step step = null;
+				if (Step.Create(node, s_namespaceManager, processingFolderPath, ref step))
+					stepList.Add(step);
+			}
+			if (stepList.Count == 0)
+				return false;
+
+			pipeline = new Pipeline(stepList);
+			return true;
+		}
+
+		Pipeline(IList<Step> stepList)
+        {
+			m_stepList = new List<Step>(stepList);
+        }
+
+		// Given inputStream, return outputStream containing results from the pipeline.
+		public void Transform(Stream inputStream, ref MemoryStream outputStream)
+		{
+			Debug.Assert(outputStream == null);
+			outputStream = new MemoryStream();
+			Debug.Assert(m_stepList.Count != 0);
+			m_stepList[0].Transform(inputStream, outputStream);
+			for (int i = 1; i != m_stepList.Count; i++)
+			{
+				MemoryStream outputStreamFromPreviousFilter = outputStream;
+				outputStream = new MemoryStream();
+				// Because the output from the current filter is the input to the next filter
+				// or the data sink, seek to the beginning of the stream.
+				inputStream.Seek(0, SeekOrigin.Begin);
+				m_stepList[i].Transform(outputStreamFromPreviousFilter, outputStream);
+			}
+		}
+
+		// Here is sample code to create a pipeline and transform data.
+		static void example()
+		{
+			// Create the pipeline.
+			string processingFolderPath = "...\\Processing";
+			string processingFilePath = Path.Combine(processingFolderPath, "Processing.xml");
+			XmlReaderSettings settings = new XmlReaderSettings();
+			XPathDocument processingDocument = new XPathDocument(XmlReader.Create(processingFilePath, settings));
+			XPathNavigator processingNavigator = processingDocument.CreateNavigator();
+			XPathNavigator processNavigator = processingNavigator.SelectSingleNode("processing/process[@type='inventory']");
+			Pipeline pipeline = null;
+			if (!Pipeline.Create(processNavigator, processingFolderPath, ref pipeline))
+				return;
+
+			// Use the pipeline.
+			Stream inputStream = null; // Represents what the program produces.
+			MemoryStream outputStream = null;
+			pipeline.Transform(inputStream, ref outputStream);
+			string outputFilePath = "...\\PROJECT.PhoneticInventory.xml";
+			using (Stream outputFileStream = new FileStream(outputFilePath, FileMode.Create))
+			{
+				outputStream.WriteTo(outputFileStream);
+				outputStream.Close();
+			}
+		}
+	}
+}
---
+++ src/Pa/Processing/Step.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace SIL.Pa.Processing
+{
+	/// <summary>
+	/// Corresponds to one xslt element in the Processing.xml file.
+	/// </summary>
+	public class Step
+	{
+		private string m_xsltFilePath;
+		private XslCompiledTransform m_xslt;
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="navigator">The parent pipeline element containing the xslt
+		/// elements.</param>
+		/// <param name="namespaceManager">In the Processing.xml file, each pipeline and its
+		/// children are in the XProc namespace.</param>
+		/// <param name="processingFolderPath">Full path to folder containing Processing.xml
+		/// and *.xsl files.</param>
+		/// <param name="step">Initially null, returns the object if this function
+		/// returns true.</param>
+		/// ------------------------------------------------------------------------------------
+		static public bool Create(XPathNavigator navigator,
+			XmlNamespaceManager namespaceManager, string processingFolderPath, ref Step step)
+		{
+			Debug.Assert(step == null);
+			
+			XPathNavigator documentNavigator =
+				navigator.SelectSingleNode("p:input[@port='stylesheet']/p:document[@href]", namespaceManager);
+			
+			if (documentNavigator == null)
+				return false;
+
+			string xsltFileName = documentNavigator.GetAttribute("href", string.Empty); // No namespace for attributes.
+			string xsltFilePath = Path.Combine(processingFolderPath, xsltFileName);
+			XslCompiledTransform xslt = new XslCompiledTransform();
+
+			try
+			{
+				// TO DO: If you enable the document() function, restrict the resources that can be accessed
+				// by passing an XmlSecureResolver object to the Transform method.
+				// The XmlResolver used to resolve any style sheets referenced in XSLT import and include elements.
+				XsltSettings settings = new XsltSettings(true, false);
+				XmlUrlResolver resolver = new XmlUrlResolver();
+				resolver.Credentials = null;
+				xslt.Load(xsltFilePath, settings, null);
+			}
+			catch (Exception innerException)
+			{
+				Exception exception = new Exception("Unable to build XSL Transformation filter.", innerException);
+				exception.Data.Add("XSL Transformation file path", xsltFilePath);
+				throw exception;
+			}
+
+			step = new Step(xsltFilePath, xslt);
+			return true;
+		}
+
+		Step(string xsltFilePath, XslCompiledTransform xslt)
+		{
+			m_xsltFilePath = xsltFilePath;
+			m_xslt = xslt;
+		}
+
+		// Given inputStream and outputStream, do one XSL Transformation step.
+		public void Transform(Stream inputStream, MemoryStream outputStream)
+		{
+			XmlReaderSettings readerSettings = new XmlReaderSettings();
+			readerSettings.ProhibitDtd = false; // If true, it throws an exception if there is a DTD.
+			readerSettings.ValidationType = ValidationType.None;
+			XmlReader inputXML = XmlReader.Create(inputStream, readerSettings);
+
+			// OutputSettings corresponds to the xsl:output element of the style sheet.
+			XmlWriterSettings writerSettings = m_xslt.OutputSettings.Clone();
+			if (writerSettings.Indent) // Cause indent="true" to insert line breaks but not tabs.
+				writerSettings.IndentChars = ""; // Even if the document element is html.
+
+			XmlWriter outputXML = XmlWriter.Create(outputStream, writerSettings);
+			try
+			{
+				m_xslt.Transform(inputXML, outputXML);
+			}
+			catch (Exception innerException)
+			{
+				Exception exception = new Exception("Unable to convert using XSL Transformation filter.", innerException);
+				exception.Data.Add("XSL Transformation file path", m_xsltFilePath);
+				throw exception;
+			}
+			finally
+			{
+				inputXML.Close();
+				outputXML.Flush(); // The next filter or the data sink will close the stream
+			}
+		}
+	}
+}
