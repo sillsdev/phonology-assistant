876146c1be18760fb47ecbc67f19fe6e5877ff81
david_olson <devnull@localhost>
2010-05-14 18:04:29 +0000

    Moved some stuff around.

---
+++ src/SilUtils/PortableSettingsProvider/FormSettings.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace SilUtils
+{
+	/// ----------------------------------------------------------------------------------------
+	/// <summary>
+	/// 
+	/// </summary>
+	/// ----------------------------------------------------------------------------------------
+	[XmlType("windowSettings")]
+	public class FormSettings
+	{
+		[XmlElement("state")]
+		public FormWindowState State { get; set; }
+		[XmlElement("bounds")]
+		public string SerializedBounds { get; set; }
+		[XmlElement("dpi")]
+		public float DPI { get; set; }
+			
+		private readonly float m_currDpi;
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		public FormSettings()
+		{
+			Bounds = new Rectangle(0, 0, 0, -1);
+			State = FormWindowState.Normal;
+			using (Form frm = new Form())
+			using (Graphics g = frm.CreateGraphics())
+				m_currDpi = DPI = g.DpiX;
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		[XmlIgnore]
+		public Rectangle Bounds
+		{
+			get
+			{
+				var bnds = PortableSettingsProvider.GetIntArrayFromString(SerializedBounds);
+				var rc = new Rectangle(0, 0, 0, -1);
+
+				if (bnds.Length >= 1)
+					rc.X = bnds[0];
+				if (bnds.Length >= 2)
+					rc.Y = bnds[1];
+				if (bnds.Length >= 3)
+					rc.Width = bnds[2];
+				if (bnds.Length >= 4)
+					rc.Height = bnds[3];
+
+				return rc;
+			}
+			set
+			{
+				SerializedBounds = string.Format("{0}, {1}, {2}, {3}",
+					value.X, value.Y, value.Width, value.Height);
+			}
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		public static FormSettings Create(Form frm)
+		{
+			var formSettings = new FormSettings();
+			formSettings.InitializeForm(frm);
+			return formSettings; 
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		public void InitializeForm(Form frm)
+		{
+			frm.ResizeEnd += HandleFormResizeEnd;
+			frm.LocationChanged += HandleFormLocationChanged;
+			frm.HandleDestroyed += HandleFormHandleDestroyed;
+
+			if (Bounds.Height <= 0 || DPI != m_currDpi)
+			{
+				frm.StartPosition = FormStartPosition.CenterScreen;
+				DPI = m_currDpi;
+			}
+			else
+			{
+				frm.StartPosition = FormStartPosition.Manual;
+				frm.Bounds = Bounds;
+
+				if (frm.WindowState != State)
+					frm.WindowState = State;
+			}
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		private void HandleFormLocationChanged(object sender, EventArgs e)
+		{
+			HandleFormResizeEnd(sender, e);
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		private void HandleFormResizeEnd(object sender, EventArgs e)
+		{
+			var frm = sender as Form;
+
+			if (frm.WindowState == FormWindowState.Normal)
+				Bounds = frm.Bounds;
+			
+			if (frm.WindowState != FormWindowState.Minimized)
+				State = frm.WindowState;
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		void HandleFormHandleDestroyed(object sender, EventArgs e)
+		{
+			var frm = sender as Form;
+			frm.ResizeEnd -= HandleFormResizeEnd;
+			frm.LocationChanged -= HandleFormLocationChanged;
+			frm.HandleDestroyed -= HandleFormHandleDestroyed;
+		}
+	}
+}
---
+++ src/SilUtils/PortableSettingsProvider/GridSettings.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace SilUtils
+{
+	/// ----------------------------------------------------------------------------------------
+	/// <summary>
+	/// 
+	/// </summary>
+	/// ----------------------------------------------------------------------------------------
+	[XmlType("gridSettings")]
+	public class GridSettings
+	{
+		[XmlElement("columnHeaderHeight")]
+		public int ColumnHeaderHeight { get; set; }
+		[XmlElement("dpi")]
+		public float DPI { get; set; }
+		[XmlArray("columns"), XmlArrayItem("col")]
+		public GridColumnSettings[] Columns { get; set; }
+
+		private readonly float m_currDpi;
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		public GridSettings()
+		{
+			ColumnHeaderHeight = -1;
+			Columns = new GridColumnSettings[] { };
+			using (Form frm = new Form())
+			using (Graphics g = frm.CreateGraphics())
+				m_currDpi = DPI = g.DpiX;
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		public static GridSettings Create(DataGridView grid)
+		{
+			var gridSettings = new GridSettings();
+
+			gridSettings.ColumnHeaderHeight = grid.ColumnHeadersHeight;
+
+			var colSettings = new List<GridColumnSettings>();
+
+			foreach (DataGridViewColumn col in grid.Columns)
+			{
+				colSettings.Add(new GridColumnSettings
+				{
+					Id = col.Name,
+					Width = col.Width,
+					Visible = col.Visible,
+					DisplayIndex = col.DisplayIndex
+				});
+			}
+
+			gridSettings.Columns = colSettings.ToArray();
+			return gridSettings;
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		public void InitializeGrid(DataGridView grid)
+		{
+			foreach (var col in Columns)
+			{
+				grid.Columns[col.Id].Visible = col.Visible;
+
+				if (col.Width >= 0)
+					grid.Columns[col.Id].Width = col.Width;
+
+				if (col.DisplayIndex >= 0)
+					grid.Columns[col.Id].DisplayIndex = col.DisplayIndex;
+			}
+
+			// If the column header height or the former dpi settings are different,
+			// then auto. calculate the height of the column headings.
+			if (ColumnHeaderHeight <= 0 || DPI != m_currDpi)
+				grid.AutoResizeColumnHeadersHeight();
+			else
+				grid.ColumnHeadersHeight = ColumnHeaderHeight;
+		}
+	}
+
+	/// ----------------------------------------------------------------------------------------
+	/// <summary>
+	/// 
+	/// </summary>
+	/// ----------------------------------------------------------------------------------------
+	public class GridColumnSettings
+	{
+		[XmlAttribute("id")]
+		public string Id { get; set; }
+		[XmlAttribute("visible")]
+		public bool Visible { get; set; }
+		[XmlAttribute("width")]
+		public int Width { get; set; }
+		[XmlAttribute("displayIndex")]
+		public int DisplayIndex { get; set; }
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// 
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		internal GridColumnSettings()
+		{
+			Visible = true;
+			Width = -1;
+			DisplayIndex = -1;
+		}
+	}
+}
