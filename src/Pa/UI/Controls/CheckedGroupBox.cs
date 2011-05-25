e16e564b81880dcb8f8a9a81c9b6d531b88d4f35
olson <devnull@localhost>
2011-05-25 09:01:15 -0400

    This is in preparation for redoing the search options drop-down.

---
+++ src/Pa/UI/Controls/CheckedGroupBox.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SIL.Pa.UI.Controls
+{
+	public partial class CheckedGroupBox : UserControl
+	{
+		public event EventHandler CheckedChanged;
+		private Padding _padding = new Padding(0);
+
+		/// ------------------------------------------------------------------------------------
+		public CheckedGroupBox()
+		{
+			InitializeComponent();
+
+			_checkBox.CheckedChanged += delegate
+			{
+				if (CheckedChanged != null)
+					CheckedChanged(this, EventArgs.Empty);
+			};
+		}
+
+		/// ------------------------------------------------------------------------------------
+		public override Font Font
+		{
+			get { return base.Font; }
+			set { base.Font = _checkBox.Font = _groupBox.Font = value; }
+		}
+
+		/// ------------------------------------------------------------------------------------
+		[Browsable(true)]
+		public override string Text
+		{
+			get { return base.Text; }
+			set { base.Text = _checkBox.Text = value; }
+		}
+
+		/// ------------------------------------------------------------------------------------
+		public new Padding Padding
+		{
+			get { return _padding; }
+			set { _padding = _groupBox.Padding = value; }
+		}
+
+		/// ------------------------------------------------------------------------------------
+		[Browsable(false)]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public bool Checked
+		{
+			get { return _checkBox.Checked; }
+			set { _checkBox.Checked = value; }
+		}
+	}
+}
