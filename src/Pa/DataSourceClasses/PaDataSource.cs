18003cb1144b3ed41e78371261dc0c457f5d8085
jamesprabud <jamesprabud@gmail.com>
2015-02-25 12:57:11 +0530

    License added for excluded files for PA-1315

--- src/Pa/DataSourceClasses/PaDataSource.cs
+++ src/Pa/DataSourceClasses/PaDataSource.cs
@@ -6,6 +6,15 @@ using System.Xml;
 using System.Xml.Serialization;
 using Localization;
 using Palaso.Reporting;
+// ---------------------------------------------------------------------------------------------
+#region // Copyright (c) 2005-2015, SIL International.
+// <copyright from='2005' to='2015' company='SIL International'>
+//		Copyright (c) 2005-2015, SIL International.
+//    
+//		This software is distributed under the MIT License, as specified in the LICENSE.txt file.
+// </copyright> 
+#endregion
+// 
 using SIL.Pa.DataSource.FieldWorks;
 using SIL.Pa.DataSource.Sa;
 using SIL.Pa.Model;
