167b8cf5188a3afb9ac920bc1d97c57cbd06f447
gtryus <gtryus@gmail.com>
2016-11-01 13:34:35 -0500

    Update to fix crash and brand installer

--- src/Pa/Program.cs
+++ src/Pa/Program.cs
@@ -1,11 +1,15 @@
 // ---------------------------------------------------------------------------------------------
+
 #region // Copyright (c) 2005-2015, SIL International.
+
 // <copyright from='2005' to='2015' company='SIL International'>
 //		Copyright (c) 2005-2015, SIL International.
 //    
 //		This software is distributed under the MIT License, as specified in the LICENSE.txt file.
 // </copyright> 
+
 #endregion
+
 // 
 using System;
 using System.Windows.Forms;
@@ -15,19 +19,26 @@ using SilTools;
 
 namespace SIL.Pa
 {
-	/// ----------------------------------------------------------------------------------------
-	public class Program
-	{
-		/// ------------------------------------------------------------------------------------
-		[STAThread]
-		static void Main() 
-		{
-			ErrorReport.EmailAddress = "PaFeedback@sil.org";
+    /// ----------------------------------------------------------------------------------------
+    public class Program
+    {
+        /// ------------------------------------------------------------------------------------
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                Application.SetCompatibleTextRenderingDefault(false);
+            }
+            catch
+            {
+                // The compatible rendering system may default to false anyway.
+            }
+            ErrorReport.EmailAddress = "PaFeedback@sil.org";
 			ErrorReport.AddStandardProperties();
 			ExceptionHandler.Init();
 
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
+            Application.EnableVisualStyles();
 			Application.Run(new PaMainWnd(true));
 		}
 	}
